88d0015db77d50d59c26859f9edcc84a5e4f578e
cold607 <minahossam607@gmail.com>
2026-04-18 00:31:11 +0200

    test structs

--- CoWorkingSpaceFinder.cpp
+++ CoWorkingSpaceFinder.cpp
@@ -1,5 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+struct user {
+	int Id;
+	string UserName, Password, Email, Phone;
+	bool IsAdmin;
+};
+
+struct space {
+	int SpaceId, PricePerHour, NoOfSeatAvailable;
+	float Rating;
+	string Name, Area;
+	bool HasWifi, HasMeetingRoom;
+};
+
+struct booking {
+	string date;
+	int BookingId, UserId, SpaceId, Hours, TotalCost, Seats;
+};
+
 int main(){
 
 }
