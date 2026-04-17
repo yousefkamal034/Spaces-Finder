bbfb8620a29b33843bbfd7d220c2c5aa962928da
Youssef ElDahshan <DahshanJr@gmail.com>
2026-04-17 02:03:36 +0200

    Fresh start for the project.

--- CoWorkingSpaceFinder.cpp
+++ CoWorkingSpaceFinder.cpp
@@ -1,18 +1,5 @@
-//Hello
 #include<iostream>
 using namespace std;
 int main(){
-    // silksong propaganda
-    // i love larping over persona
-    // i love larping over computer science i promise everyone that i'll vibe code
-    // i just fucked over musef
-    // elden ring propaganda
-    // tits >> ass
-    // what about personality
-    // yeah it's better that tits ngl
-    // you're wrong lmao
-    // monky hunky 
-    // kamal sucks at hosting git shit
-    // fala7en ya gd3an
-    // my ass is good
+
 }
