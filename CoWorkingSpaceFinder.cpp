5ae7025729f7dd1d97942b4ab8c5b324d4e71c8c
cold607 <minahossam607@gmail.com>
2026-04-18 02:32:50 +0200

    Revert "commiting my shit locally"
    This reverts commit aab6163ea9aa1b87e84a9c7d61bf2e11b7346b7d.

--- CoWorkingSpaceFinder.cpp
+++ CoWorkingSpaceFinder.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
-#include "structs.h"
 using namespace std;
 
+struct user {
+	int Id;
+	string UserName, Password, Email, Phone;
+	bool IsAdmin;
+};
+
+struct space {
+	int SpaceId, PricePerHour, NoOfSeatAvailable;
+	float Rating;
+	string Name, Area;
+	bool HasWifi, HasMeetingRoom;
+};
+
+struct booking {
+	string date;
+	int BookingId, UserId, SpaceId, Hours, TotalCost, Seats;
+};
 
 int main(){
-    User user;
-    user.Id = 123;
-    cout << user.Id;
-    // a7a
-
-    int holder;
-    cin >> holder;
-
+    cout << "Hello, World" << endl;
 }
